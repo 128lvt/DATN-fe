5fc6ef7b4e38f49597cb484b5cb55e3119aa0496
128lvt <128vanthoai@gmail.com>
2024-10-08 23:55:32 +0700

    carousel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import webpack from "webpack";
+const nextConfig = {
+  webpack: (config) => {
+    // Cấu hình Webpack để cung cấp jQuery
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery", // Cung cấp jQuery cho toàn cục
+      })
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import Carousel from "../components/main/carousel";
+import dynamic from "next/dynamic";
+
 // import React, { useEffect } from "react";
 // import AppTable from "../components/app.table";
 // import useSWR from "swr";
 
 export default function page() {
+  const Carousel = dynamic(() => import("../components/main/carousel"), {
+    ssr: false,
+  });
+
   // const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
   // const { data, error, isLoading } = useSWR(
@@ -25,6 +32,7 @@ export default function page() {
 
   return (
     <>
+      <Carousel />
       {/* <AppTable blogs={data?.sort((a: any, b: any) => b.id - a.id)} /> */}
     </>
   );
