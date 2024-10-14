e425846ede18e7f337d88832dd91bb5b8c4ca342
128lvt <128vanthoai@gmail.com>
2024-10-14 17:07:50 +0700

    fix

--- src/app/components/app.table.tsx
+++ src/app/components/app.table.tsx
@@ -1,81 +1,81 @@
-import Table from "react-bootstrap/Table";
-import { Button } from "react-bootstrap";
-import BlogModal from "./blog.modal";
-import { useState } from "react";
+// import Table from "react-bootstrap/Table";
+// import { Button } from "react-bootstrap";
+// import BlogModal from "./blog.modal";
+// import { useState } from "react";
 
-interface IProps {
-  blogs: IBlog[];
-}
+// interface IProps {
+//   blogs: IBlog[];
+// }
 
-const AppTable = (props: IProps) => {
-  const { blogs } = props;
+// const AppTable = (props: IProps) => {
+//   const { blogs } = props;
 
-  const [showModalCreate, setShowModalCreate] = useState<boolean>(false);
-  const [modalAction, setModalAction] = useState<"add" | "edit">("add");
-  const [currentBlog, setCurrentBlog] = useState<IBlog | undefined>(undefined);
+//   const [showModalCreate, setShowModalCreate] = useState<boolean>(false);
+//   const [modalAction, setModalAction] = useState<"add" | "edit">("add");
+//   const [currentBlog, setCurrentBlog] = useState<IBlog | undefined>(undefined);
 
-  const handleAddModal = () => {
-    setModalAction("add");
-    setCurrentBlog(undefined);
-    setShowModalCreate(true);
-  };
+//   const handleAddModal = () => {
+//     setModalAction("add");
+//     setCurrentBlog(undefined);
+//     setShowModalCreate(true);
+//   };
 
-  const handleEditModal = (blog: IBlog) => {
-    setModalAction("edit");
-    setCurrentBlog(blog);
-    setShowModalCreate(true);
-  };
-  return (
-    <>
-      <div
-        className="mb-3"
-        style={{ display: "flex", justifyContent: "space-between" }}
-      >
-        <h3>Table Blogs</h3>
-        <Button variant="secondary" onClick={() => handleAddModal()}>
-          Add New
-        </Button>
-      </div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {blogs.map((blog) => {
-            return (
-              <tr key={blog.id}>
-                <td>{blog.id}</td>
-                <td>{blog.title}</td>
-                <td>{blog.author}</td>
-                <td>
-                  <Button>View</Button>
-                  <Button
-                    variant="warning"
-                    className="mx-3"
-                    onClick={() => handleEditModal(blog)}
-                  >
-                    Edit
-                  </Button>
-                  <Button variant="danger">Delete</Button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-      <BlogModal
-        action={modalAction}
-        showModalCreate={showModalCreate}
-        setShowModalCreate={setShowModalCreate}
-        currentBlog={currentBlog}
-      />
-    </>
-  );
-};
+//   const handleEditModal = (blog: IBlog) => {
+//     setModalAction("edit");
+//     setCurrentBlog(blog);
+//     setShowModalCreate(true);
+//   };
+//   return (
+//     <>
+//       <div
+//         className="mb-3"
+//         style={{ display: "flex", justifyContent: "space-between" }}
+//       >
+//         <h3>Table Blogs</h3>
+//         <Button variant="secondary" onClick={() => handleAddModal()}>
+//           Add New
+//         </Button>
+//       </div>
+//       <Table striped bordered hover>
+//         <thead>
+//           <tr>
+//             <th>#</th>
+//             <th>Title</th>
+//             <th>Author</th>
+//             <th>Action</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {blogs.map((blog) => {
+//             return (
+//               <tr key={blog.id}>
+//                 <td>{blog.id}</td>
+//                 <td>{blog.title}</td>
+//                 <td>{blog.author}</td>
+//                 <td>
+//                   <Button>View</Button>
+//                   <Button
+//                     variant="warning"
+//                     className="mx-3"
+//                     onClick={() => handleEditModal(blog)}
+//                   >
+//                     Edit
+//                   </Button>
+//                   <Button variant="danger">Delete</Button>
+//                 </td>
+//               </tr>
+//             );
+//           })}
+//         </tbody>
+//       </Table>
+//       <BlogModal
+//         action={modalAction}
+//         showModalCreate={showModalCreate}
+//         setShowModalCreate={setShowModalCreate}
+//         currentBlog={currentBlog}
+//       />
+//     </>
+//   );
+// };
 
-export default AppTable;
+// export default AppTable;
--- src/app/components/blog.modal.tsx
+++ src/app/components/blog.modal.tsx
@@ -1,155 +1,155 @@
-"use client";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import { mutate } from "swr";
+// "use client";
+// import Button from "react-bootstrap/Button";
+// import Modal from "react-bootstrap/Modal";
+// import Form from "react-bootstrap/Form";
+// import { useEffect, useState } from "react";
+// import { toast } from "react-toastify";
+// import { mutate } from "swr";
 
-interface IProps {
-  action: "add" | "edit";
-  showModalCreate: boolean;
-  setShowModalCreate: (value: boolean) => void;
-  currentBlog?: IBlog;
-}
+// interface IProps {
+//   action: "add" | "edit";
+//   showModalCreate: boolean;
+//   setShowModalCreate: (value: boolean) => void;
+//   currentBlog?: IBlog;
+// }
 
-function CreateModal(props: IProps) {
-  const { action, showModalCreate, setShowModalCreate, currentBlog} = props;
+// function CreateModal(props: IProps) {
+//   const { action, showModalCreate, setShowModalCreate, currentBlog} = props;
 
-  const [title, setTitle] = useState<string>("");
-  const [author, setAuthor] = useState<string>("");
-  const [content, setContent] = useState<string>("");
+//   const [title, setTitle] = useState<string>("");
+//   const [author, setAuthor] = useState<string>("");
+//   const [content, setContent] = useState<string>("");
 
-  useEffect(() => {
-    if (action === "edit" && currentBlog) {
-      setTitle(currentBlog.title);
-      setAuthor(currentBlog.author);
-      setContent(currentBlog.content);
-    } else {
-      setTitle("");
-      setAuthor("");
-      setContent("");
-    }
-  }, [action, currentBlog]);
+//   useEffect(() => {
+//     if (action === "edit" && currentBlog) {
+//       setTitle(currentBlog.title);
+//       setAuthor(currentBlog.author);
+//       setContent(currentBlog.content);
+//     } else {
+//       setTitle("");
+//       setAuthor("");
+//       setContent("");
+//     }
+//   }, [action, currentBlog]);
 
-  const handleSave = () => {
-    if (!title || !author || !content) {
-      toast.error("Please fill in all fields");
-      return;
-    }
-    switch (action) {
-      case "edit":
-        {
-          fetch(`http://localhost:8000/blogs/${currentBlog?.id}`, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              title,
-              author,
-              content,
-            }),
-          })
-            .then((res) => res.json())
-            .then((res) => {
-              if (res) {
-                toast.success("Blog updated successfully");
-                handleCloseModal();
-                mutate("http://localhost:8000/blogs");
-              }
-            });
-        }
-        break;
-      case "add":
-        {
-          fetch("http://localhost:8000/blogs", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              title,
-              author,
-              content,
-            }),
-          })
-            .then((res) => res.json())
-            .then((res) => {
-              if (res) {
-                toast.success("Create success");
-                handleCloseModal();
-                mutate("http://localhost:8000/blogs");
-              }
-            });
-        }
-        break;
-    }
-  };
+//   const handleSave = () => {
+//     if (!title || !author || !content) {
+//       toast.error("Please fill in all fields");
+//       return;
+//     }
+//     switch (action) {
+//       case "edit":
+//         {
+//           fetch(`http://localhost:8000/blogs/${currentBlog?.id}`, {
+//             method: "PUT",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//               title,
+//               author,
+//               content,
+//             }),
+//           })
+//             .then((res) => res.json())
+//             .then((res) => {
+//               if (res) {
+//                 toast.success("Blog updated successfully");
+//                 handleCloseModal();
+//                 mutate("http://localhost:8000/blogs");
+//               }
+//             });
+//         }
+//         break;
+//       case "add":
+//         {
+//           fetch("http://localhost:8000/blogs", {
+//             method: "POST",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify({
+//               title,
+//               author,
+//               content,
+//             }),
+//           })
+//             .then((res) => res.json())
+//             .then((res) => {
+//               if (res) {
+//                 toast.success("Create success");
+//                 handleCloseModal();
+//                 mutate("http://localhost:8000/blogs");
+//               }
+//             });
+//         }
+//         break;
+//     }
+//   };
 
-  const handleCloseModal = () => {
-    setTitle("");
-    setAuthor("");
-    setContent("");
-    setShowModalCreate(false);
-  };
+//   const handleCloseModal = () => {
+//     setTitle("");
+//     setAuthor("");
+//     setContent("");
+//     setShowModalCreate(false);
+//   };
 
-  return (
-    <>
-      <Modal
-        show={showModalCreate}
-        onHide={() => setShowModalCreate(false)}
-        backdrop="static"
-        keyboard={false}
-        size="lg"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>
-            {action === "add" ? "Add Blog" : "Edit Blog"}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3">
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="..."
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Author</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="..."
-                value={author}
-                onChange={(e) => setAuthor(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Content</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                value={content}
-                onChange={(e) => setContent(e.target.value)}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => handleCloseModal()}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={() => handleSave()}>
-            {action === "add" ? "Add" : "Update"}
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Modal
+//         show={showModalCreate}
+//         onHide={() => setShowModalCreate(false)}
+//         backdrop="static"
+//         keyboard={false}
+//         size="lg"
+//       >
+//         <Modal.Header closeButton>
+//           <Modal.Title>
+//             {action === "add" ? "Add Blog" : "Edit Blog"}
+//           </Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body>
+//           <Form>
+//             <Form.Group className="mb-3">
+//               <Form.Label>Title</Form.Label>
+//               <Form.Control
+//                 type="text"
+//                 placeholder="..."
+//                 value={title}
+//                 onChange={(e) => setTitle(e.target.value)}
+//               />
+//             </Form.Group>
+//             <Form.Group className="mb-3">
+//               <Form.Label>Author</Form.Label>
+//               <Form.Control
+//                 type="text"
+//                 placeholder="..."
+//                 value={author}
+//                 onChange={(e) => setAuthor(e.target.value)}
+//               />
+//             </Form.Group>
+//             <Form.Group className="mb-3">
+//               <Form.Label>Content</Form.Label>
+//               <Form.Control
+//                 as="textarea"
+//                 rows={3}
+//                 value={content}
+//                 onChange={(e) => setContent(e.target.value)}
+//               />
+//             </Form.Group>
+//           </Form>
+//         </Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary" onClick={() => handleCloseModal()}>
+//             Close
+//           </Button>
+//           <Button variant="primary" onClick={() => handleSave()}>
+//             {action === "add" ? "Add" : "Update"}
+//           </Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </>
+//   );
+// }
 
-export default CreateModal;
+// export default CreateModal;
