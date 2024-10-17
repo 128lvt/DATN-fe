68fe3e277557eb0f8546899c860054885393368e
128lvt <128vanthoai@gmail.com>
2024-10-17 16:46:36 +0700

    fix carousel

--- src/app/components/main/Carousel.tsx
+++ src/app/components/main/Carousel.tsx
@@ -1,9 +1,11 @@
 'use client'
 import { useEffect } from 'react'
-import OwlCarousel from 'react-owl-carousel'
+import dynamic from 'next/dynamic'
 import 'owl.carousel/dist/assets/owl.carousel.css'
 import 'owl.carousel/dist/assets/owl.theme.default.css'
-import 'owl.carousel'
+
+// Sử dụng dynamic import với ssr: false
+const OwlCarousel = dynamic(() => import('react-owl-carousel'), { ssr: false });
 
 const Carousel = () => {
   useEffect(() => {
@@ -13,101 +15,37 @@ const Carousel = () => {
       const dataSetBg = img.getAttribute('data-setbg')
 
       if (dataSetBg) {
-        ;(img as HTMLElement).style.backgroundImage = `url(${dataSetBg})`
+        (img as HTMLElement).style.backgroundImage = `url(${dataSetBg})`
       }
     })
   }, [])
 
   return (
-    <>
-      <section className="hero">
-        <OwlCarousel
-          className="hero__slider owl-carousel autoHeight"
-          loop={true}
-          margin={0}
-          items={1}
-          dots={false}
-          nav={true}
-          navText={[
-            '<div class="nav-btn prev-slide">❮</div>',
-            '<div class="nav-btn next-slide">❯</div>',
-          ]}
-          animateIn="fadeIn"
-          animateOut="fadeOut"
-          smartSpeed={1200}
-          autoplay={false}
-        >
-          <div className="hero__items set-bg" data-setbg="img/hero/hero-1.jpg">
-            <div className="container">
-              <div className="row">
-                <div className="col-xl-5 col-lg-7 col-md-8">
-                  <div className="hero__text">
-                    <h6>Summer Collection</h6>
-                    <h2>Fall - Winter Collections 2030</h2>
-                    <p>
-                      A specialist label creating luxury essentials. Ethically
-                      crafted with an unwavering commitment to exceptional
-                      quality.
-                    </p>
-                    <a href="#" className="primary-btn">
-                      Shop now <span className="arrow_right" />
-                    </a>
-                    <div className="hero__social">
-                      <a href="#">
-                        <i className="fa fa-facebook" />
-                      </a>
-                      <a href="#">
-                        <i className="fa fa-twitter" />
-                      </a>
-                      <a href="#">
-                        <i className="fa fa-pinterest" />
-                      </a>
-                      <a href="#">
-                        <i className="fa fa-instagram" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="hero__items set-bg" data-setbg="img/hero/hero-2.jpg">
-            <div className="container">
-              <div className="row">
-                <div className="col-xl-5 col-lg-7 col-md-8">
-                  <div className="hero__text">
-                    <h6>Summer Collection</h6>
-                    <h2>Fall - Winter Collections 2030</h2>
-                    <p>
-                      A specialist label creating luxury essentials. Ethically
-                      crafted with an unwavering commitment to exceptional
-                      quality.
-                    </p>
-                    <a href="#" className="primary-btn">
-                      Shop now <span className="arrow_right" />
-                    </a>
-                    <div className="hero__social">
-                      <a href="#">
-                        <i className="fa fa-facebook" />
-                      </a>
-                      <a href="#">
-                        <i className="fa fa-twitter" />
-                      </a>
-                      <a href="#">
-                        <i className="fa fa-pinterest" />
-                      </a>
-                      <a href="#">
-                        <i className="fa fa-instagram" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </OwlCarousel>
-      </section>
-    </>
+    <section className="hero">
+      <OwlCarousel
+        className="hero__slider owl-carousel autoHeight"
+        loop={true}
+        margin={0}
+        items={1}
+        dots={false}
+        nav={true}
+        navText={[
+          '<div class="nav-btn prev-slide">❮</div>',
+          '<div class="nav-btn next-slide">❯</div>',
+        ]}
+        animateIn="fadeIn"
+        animateOut="fadeOut"
+        smartSpeed={1200}
+        autoplay={false}
+      >
+        <div className="hero__items set-bg" data-setbg="img/hero/hero-1.jpg">
+          {/* Nội dung ở đây */}
+        </div>
+        <div className="hero__items set-bg" data-setbg="img/hero/hero-2.jpg">
+          {/* Nội dung ở đây */}
+        </div>
+      </OwlCarousel>
+    </section>
   )
 }
 
